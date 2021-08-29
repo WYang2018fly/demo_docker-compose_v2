d759f0e0583978dcc0ab75dd9b62424a13adb26f
汪洋 <wyang@shgbit.com>
2021-08-29 19:09:33 +0800

    fix bootstrap.sh

--- app/bootstrap.sh
+++ app/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 echo "start frontend @bootstrap"
 cat >> /var/www/my_app/runtime.config.js <<EOF
-window.SVC_TRAEFIK_URL = 'http://localhost:19007';
+window.SVC_TRAEFIK_URL = '$SVC_TRAEFIK_URL';
 EOF
 nginx -g "daemon off;"
